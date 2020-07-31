ea6b91b301a7d949b8eb4edf1febfcedbf92fdb5
Anwesh43 <anweshthecool0@gmail.com>
2020-08-01 03:02:25 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedmiroredstrokedboxview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedmiroredstrokedboxview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedmiroredstrokedboxview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.mirrorstrokedboxview.MirroredStrokedBoxView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MirroredStrokedBoxView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
