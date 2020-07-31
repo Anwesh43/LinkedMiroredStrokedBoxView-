5f38c6a1c03d4a881880850d7698589dad2c6d77
Anwesh43 <anweshthecool0@gmail.com>
2020-08-01 02:39:55 +0530

    adding MirroredStrokedBoxView class

--- mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
+++ mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawMSBNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class MirroredStrokedBoxView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
