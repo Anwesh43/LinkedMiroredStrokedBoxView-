19118138c657e83276ca2e1e33fdf084d6d7c106
Anwesh43 <anweshthecool0@gmail.com>
2020-08-01 02:27:41 +0530

    adding constants and libraries

--- mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
+++ mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.mirrorstrokedboxview
 /**
  * Created by anweshmishra on 01/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.RectF
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#4CAF50", "#F44336", "#03A9F4", "#FF9800", "#3F51B5")
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.5f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 90f
