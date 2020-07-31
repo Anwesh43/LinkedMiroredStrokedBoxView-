c6d603fcf4ee1559d5f4f7fd5c00de9fe8c21836
Anwesh43 <anweshthecool0@gmail.com>
2020-08-01 02:47:14 +0530

    adding MSBNode class

--- mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
+++ mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
@@ -117,4 +117,45 @@ class MirroredStrokedBoxView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MBSNode(var i : Int, val state : State = State()) {
+
+        private var next : MBSNode? = null
+        private var prev : MBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MBSNode {
+            var curr : MBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
