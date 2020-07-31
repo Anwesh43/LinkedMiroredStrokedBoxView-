ebba6c53efe0640528c19777aa22616e2635160b
Anwesh43 <anweshthecool0@gmail.com>
2020-08-01 02:38:42 +0530

    adding Canvas extension method

--- mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
+++ mirrorstrokedboxview/src/main/java/com/anwesh/uiprojects/mirrorstrokedboxview/MirroredStrokedBoxView.kt
@@ -20,9 +20,37 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.5f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
-val rot : Float = 90f
+val rot : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawMirrorStrokeBox(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    scale(1f - 2 * i, 1f - 2 * i)
+    rotate(rot * sf3)
+    drawLine(0f, 0f, 0f, size * sf1, paint)
+    drawRect(RectF(-size * sf2, 0f, 0f, size), paint)
+    restore()
+}
+
+fun Canvas.drawMSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        drawMirrorStrokeBox(i, scale, w, h, paint)
+    }
+    restore()
+}
